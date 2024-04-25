5adc2e91c60ba43745b07ff5fa7f79881854a7d9
hargun0360 <hargunsingh0720@gmail.com>
2024-04-25 13:59:48 +0530

    Connnect Database

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+dotenv.config();
+
+const app = express();
+const DB = process.env.MONGO_URL;
+console.log(DB);
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log("connection established");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+app.get("/", (req, res) => {    
+    res.send("Hello World!");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
